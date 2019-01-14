c883e9e729ed33e09142fd1fcbb940bd0bbf9bd0
devManWeb <43416629+devManWeb@users.noreply.github.com>
2019-01-14 18:55:37 +0100

    Added archive_zip

---
+++ archive_zip.py
@@ -0,0 +1,28 @@
+from datetime import date
+import shutil
+import os
+
+'''Variables'''
+def variables():
+	today_date = str(date.today())
+	file_name = today_date.replace("-", "")
+
+	folder_name = "C:/Work"
+
+	return file_name,folder_name
+
+
+try:
+	'''Compressing'''
+	print("Compressing " + variables()[0] +".zip ...")
+	shutil.make_archive(variables()[0], 'zip', variables()[1])
+
+	'''Moving'''
+	print("Moving " + variables()[0] +".zip ...")
+	shutil.move(variables()[0] + '.zip', variables()[1])
+
+except Exception as e:
+	'''Error managment from shutil operations'''
+	print(e)
+	input("Press any key to exit...")
+
