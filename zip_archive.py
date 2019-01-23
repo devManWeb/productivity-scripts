e82bc03e80b6b1d4ff88aacc2dc06f46f4d3ae9e
Leonardo Bissaro <leonbissaro@gmail.com>
2019-01-23 18:52:37 +0100

    Added features and corrected some bugs

--- zip_archive.py
+++ zip_archive.py
@@ -1,20 +1,20 @@
 '''
 This script creates a zip archive of the contents of a selected directory
-The name of the zip file is the combination of the current year, month and day
-the zip archive is then moved to the selected directory
+The file name of the zip is the union of the current year, month, day, hours and minutes
+the archive is then moved to the selected directory
 '''
 
-from datetime import date
 import shutil
 import os
+import time
 
 class Zip_maker():
 
-	def __init__(self, folder_name):
+	def __init__(self, folder_name, destination):
 		self.folder_name = folder_name
-		self.compression_ok = True	
-		self.today_date = str(date.today())
-		self.file_name = self.today_date.replace("-", "")
+		self.destination = destination
+		self.compression_ok = True
+		self.file_name = time.strftime("%Y%m%d-%H%M")
 
 	def compress(self):
 		print("Compressing " + self.file_name +".zip ...")
@@ -22,8 +22,8 @@ class Zip_maker():
 
 	def get_compression_ok(self):
 		return self.compression_ok
-		
-	def set_compression_ok(value,bool):
+
+	def set_compression_ok(self,bool):
 		self.compression_ok = bool
 
 	def compress_error(self):
@@ -32,30 +32,29 @@ class Zip_maker():
 		input("Press any key to exit...")
 
 	def move(self):
-		print("Moving " + self.file_name +".zip ...")
-		shutil.move(self.file_name + '.zip', self.folder_name)
+		print("Moving " + self.file_name +".zip to " + self.destination + "...")
+		shutil.move(self.file_name + '.zip', self.destination)
 
 	def move_error(self):
 		input("Press any key to exit...")
-	
 
-data = Zip_maker("D:/Sviluppo") #Edit this with the path of your folder
+
+#Edit this with the path of your folder and with the destination folder
+data = Zip_maker("D:/Sviluppo", "D:/backups")
 
 try:
 	data.compress()
-	
+
 except Exception as comp_error:
         data.set_compression_ok(False)
         print(comp_error)
         data.compress_error()
 
-if data.get_compression_ok: 	
+if data.get_compression_ok():
 
 	try:
 		data.move()
-		
+
 	except Exception as mov_error:
 		print(mov_error)
 		data.move_error()
-
-
