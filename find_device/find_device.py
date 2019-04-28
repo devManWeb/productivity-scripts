35e7c17dc1153b68352993384db9354fd9d69179
Leonardo Bissaro <leonbissaro@gmail.com>
2019-04-28 12:07:31 +0200

    fixed some bugs

--- find_device/find_device.py
+++ find_device/find_device.py
@@ -1,17 +1,14 @@
 '''
 Search for devices in the local network that have the same first three pairs of the MAC address.
-Then, if we find a corresponding device, we open it in the browser
-If we found more than one devices, we make a choice.
+Then, if we find one or more corresponding device, we show the IP
 The configuration parameters are loaded from the INI file
 Important: this script was developed for Win OS only
 '''
 import configparser
 import re
 import nmap
-import webbrowser
 
-def scan_open(https,target_mac):
-
+def scan_and_display(target_mac):
 	print("Scanning your network, please wait...")
 	config = configparser.ConfigParser()
 	config.read("config_file.ini")
@@ -29,42 +26,19 @@ def scan_open(https,target_mac):
 			if target_mac == mac_str:
 				device_IP.append(nm[host]["addresses"]["ipv4"])
 
-	def choose_one():
-		#if we have more than one device with the same initial MAC address
-		print("\nPlease choose an IP address:")
+	if len(device_IP) == 0:
+		print("\nNo result!")
+		start()
+	else:
+		print("\nIP address list:")
 		for item in range (0,len(device_IP)):
 			print(str(item) + " - " + device_IP[item])
-		user_choice = input("\nYour choice: ")
-		try:
-			user_choice = int(user_choice)
-			if 0 <= user_choice <= len(device_IP):
-				if https == True:
-					print("https://" + device_IP[user_choice])
-				else:
-					webbrowser.open("http://" + device_IP[user_choice], new = 2)
-			else:
-				print("Not a valid choice!")
-				choose_one()
-		except ValueError: #if the inserted value is not a int number
-				print("Not a valid choice!")
-				choose_one()
-
-	if len(device_IP) == 0:
-		print("No result!")
-		start()
-	if len(device_IP) == 1:
-		if https == True:
-			webbrowser.open(device_IP[0], new = 2)
-		else:
-			webbrowser.open(device_IP[0], new = 2)
-	else:
-		choose_one()
 
 def start():
 	config = configparser.ConfigParser()
 	config.read("config_file.ini")
 	cfg_list = config.sections()
-	user_text = ""
+	user_text = "\n"
 	position = 0
 	for device in config.sections():
 		device = re.sub("[^a-zA-Z0-9 ]+","", device)
@@ -73,17 +47,18 @@ def start():
 	user_text = user_text + "Other - Exit\n"
 	print(user_text)
 	user_choice = input("Your choice:")
+
 	try:
 		user_choice = int(user_choice)
 		if 0 <= user_choice <= position:
 			device_name = cfg_list[user_choice]
-			scan_open(config[device_name]["https"],config[device_name]["initial_MAC"])
+			scan_and_display(config[device_name]["initial_MAC"])
 		else:
 			quit()
 	except ValueError: #if the inserted value is not a int number
 		quit()
 
 try:
-    start()
+	start()
 except Exception as gen_error:
 	print("Attention, " + str(gen_error))
