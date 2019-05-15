b5c4d0b07aefa6149226e3f46288e28d6f553afb
climaset <info@climaset.it>
2019-05-15 18:34:42 +0200

    updated find device with new class

--- find_device/find_device.py
+++ find_device/find_device.py
@@ -7,58 +7,77 @@ Important: this script was developed for Windows only
 import configparser
 import re
 import nmap
+import webbrowser
 
-def scan_and_display(target_mac):
-	print("Scanning your network, please wait...")
-	config = configparser.ConfigParser()
-	config.read("config_file.ini")
-	nm = nmap.PortScanner()
-	nm.scan(hosts= config["DEFAULT"]["ip_range_to_scan"] , arguments="-sP")
-	host_list = nm.all_hosts()
-	device_IP = []
+class Find_device():
 
-	for host in host_list:
-		if "mac" not in nm[host]["addresses"]:  #if there is no MAC address
-			continue
+	def __init__(self):
+		self.config = configparser.ConfigParser()
+		self.config.read("config_file.ini")
+		self.MAC_arr = []
+		self.IP_found_arr = []
+
+	def reset_vars(self):
+		self.MAC_arr = []
+		self.IP_found_arr = []
+
+	def user_menu(self):	
+		cfg_list = self.config.sections()
+		user_text = ""
+		position = 0
+		self.reset_vars()
+		for device in self.config.sections(): #search for all configurations in the ini file
+			device = re.sub("[^a-zA-Z0-9 ]+","", device)
+			position = cfg_list.index(device) #numeric position in the list
+			user_text = user_text + str(position) + " - " + self.config[device]["device_name"] + " \n"
+		user_text = user_text + "Other - Exit\n"
+		print(user_text)
+		user_choice = input("Your choice:")
+		try:
+			user_choice = int(user_choice)
+			if 0 <= user_choice <= position:
+				name = cfg_list[user_choice]
+				self.MAC_arr.append(self.config[name]["MAC_0"])	
+			else:
+				quit()
+		except ValueError: 
+			#if the inserted value is not a int number
+			quit()	
+
+	def scan_network(self):
+		print("Scanning your network, please wait...")
+		nm = nmap.PortScanner()
+		nm.scan(hosts= self.config["DEFAULT"]["ip_range_to_scan"] , arguments="-sP")
+		host_list = nm.all_hosts()
+		target_mac = self.MAC_arr[0]
+
+		for host in host_list:
+			if "mac" not in nm[host]["addresses"]: 
+				#if there is no MAC address
+				continue
+			else:
+				mac_str = nm[host]['addresses']['mac']
+				mac_str = mac_str[:-9] #we keep only the first part of the MAC address
+				if target_mac == mac_str:
+					self.IP_found_arr.append(nm[host]["addresses"]["ipv4"])		
+
+	def show_results(self):
+
+		if not self.IP_found_arr:
+			#if there is no compatible device
+			print("No result!\n")
 		else:
-			mac_str = nm[host]['addresses']['mac']
-			mac_str = "\"" + mac_str[:-9] + "\"" #we keep only the first part of the MAC address
-			if target_mac == mac_str:
-				device_IP.append(nm[host]["addresses"]["ipv4"])
+			print("\nIP address list:")
+			for item in range (0,len(self.IP_found_arr)):
+				print(str(item) + " - " + self.IP_found_arr[item] + "\n")
+		input("Press any key to exit..")
 
-	if len(device_IP) == 0:
-		print("\nNo result!")
-		start()
-	else:
-		print("\nIP address list:")
-		for item in range (0,len(device_IP)):
-			print(str(item) + " - " + device_IP[item])
-
-def start():
-	config = configparser.ConfigParser()
-	config.read("config_file.ini")
-	cfg_list = config.sections()
-	user_text = "\n"
-	position = 0
-	for device in config.sections():
-		device = re.sub("[^a-zA-Z0-9 ]+","", device)
-		position = cfg_list.index(device) #numeric position in the list
-		user_text = user_text + str(position) + " - " + config[device]["device_name"] + " \n"
-	user_text = user_text + "Other - Exit\n"
-	print(user_text)
-	user_choice = input("Your choice:")
-
-	try:
-		user_choice = int(user_choice)
-		if 0 <= user_choice <= position:
-			device_name = cfg_list[user_choice]
-			scan_and_display(config[device_name]["initial_MAC"])
-		else:
-			quit()
-	except ValueError: #if the inserted value is not a int number
-		quit()
+app = Find_device()
 
 try:
-	start()
+	app.user_menu()
+	app.scan_network()
+	app.show_results()
 except Exception as gen_error:
 	print("Attention, " + str(gen_error))
+	input("Press any key to exit..")
