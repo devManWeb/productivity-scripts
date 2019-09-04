8376733c089709d9136d29f92a6b492ec4d5917d
Leonardo Bissaro <leonbissaro@gmail.com>
2019-09-04 13:18:59 +0200

    added the possibility of having more than one mac address per device

--- find_device/find_device.py
+++ find_device/find_device.py
@@ -1,8 +1,9 @@
 '''
 Search for devices in the local network that have the same first three pairs of the MAC address.
-Then, if we find one or more corresponding device, we show the IP
+Then, if we find a corresponding device, we open it in the browser
+If we found more than one devices, we make a choice.
 The configuration parameters are loaded from the INI file
-Important: this script was developed for Windows only
+Important: this script was developed for Win OS only
 '''
 import configparser
 import re
@@ -15,10 +16,12 @@ class Find_device():
 		self.config = configparser.ConfigParser()
 		self.config.read("config_file.ini")
 		self.MAC_arr = []
+		self.protocol = ""
 		self.IP_found_arr = []
 
 	def reset_vars(self):
 		self.MAC_arr = []
+		self.protocol = ""
 		self.IP_found_arr = []
 
 	def user_menu(self):	
@@ -26,6 +29,7 @@ class Find_device():
 		user_text = ""
 		position = 0
 		self.reset_vars()
+
 		for device in self.config.sections(): #search for all configurations in the ini file
 			device = re.sub("[^a-zA-Z0-9 ]+","", device)
 			position = cfg_list.index(device) #numeric position in the list
@@ -34,12 +38,23 @@ class Find_device():
 		print(user_text)
 		user_choice = input("Your choice:")
 		try:
+			
 			user_choice = int(user_choice)
 			if 0 <= user_choice <= position:
 				name = cfg_list[user_choice]
-				self.MAC_arr.append(self.config[name]["MAC_0"])	
+
+				#looks for all mac address associated with this device
+				item_list = self.config.items(name)
+				mac_regex = re.compile(r"MAC_\d")
+				for param_name in item_list:
+					to_check = (param_name[0]).upper()
+					if re.match(mac_regex,to_check) is not None:
+						self.MAC_arr.append(param_name[1])
+					
+				self.protocol = self.config[name]["protocol"]
 			else:
 				quit()
+
 		except ValueError: 
 			#if the inserted value is not a int number
 			quit()	
@@ -49,35 +64,54 @@ class Find_device():
 		nm = nmap.PortScanner()
 		nm.scan(hosts= self.config["DEFAULT"]["ip_range_to_scan"] , arguments="-sP")
 		host_list = nm.all_hosts()
-		target_mac = self.MAC_arr[0]
 
-		for host in host_list:
-			if "mac" not in nm[host]["addresses"]: 
-				#if there is no MAC address
-				continue
-			else:
-				mac_str = nm[host]['addresses']['mac']
-				mac_str = mac_str[:-9] #we keep only the first part of the MAC address
-				if target_mac == mac_str:
-					self.IP_found_arr.append(nm[host]["addresses"]["ipv4"])		
+		for current_mac in self.MAC_arr:
+			#scans the network for all for all associated mac address
 
-	def show_results(self):
+			for host in host_list:
+				if "mac" not in nm[host]["addresses"]: 
+					#if there is no MAC address
+					continue
+				else:
+					mac_str = nm[host]['addresses']['mac']
+					mac_str = mac_str[:-9] #we keep only the first part of the MAC address
+					if current_mac == mac_str:
+						self.IP_found_arr.append(nm[host]["addresses"]["ipv4"])		
+
+	def choose_and_open(self):
 
 		if not self.IP_found_arr:
 			#if there is no compatible device
 			print("No result!\n")
+			input("Press any key to exit..")
+		elif len(self.IP_found_arr) == 1:
+			#if there is one compatible device
+			webbrowser.open(self.protocol + "://" + self.IP_found_arr[0], new = 2)
 		else:
-			print("\nIP address list:")
+			#if we have more than one device with the same initial MAC address
+			print("\nPlease choose an IP address:")
 			for item in range (0,len(self.IP_found_arr)):
-				print(str(item) + " - " + self.IP_found_arr[item] + "\n")
-		input("Press any key to exit..")
+				print(str(item) + " - " + self.IP_found_arr[item])
+			user_choice = input("\nYour choice: ")
+			try:
+				user_choice = int(user_choice)
+				if 0 <= user_choice <= len(self.IP_found_arr):
+					webbrowser.open(self.protocol + "://" + self.IP_found_arr[user_choice], new = 2)
+				else:
+					print("Not a valid choice!")
+					self.choose_and_open()
+			except ValueError: 
+					#if the inserted value is not a int number
+					print("Not a valid choice!")
+					self.choose_and_open()
+
 
 app = Find_device()
 
 try:
 	app.user_menu()
 	app.scan_network()
-	app.show_results()
+	app.choose_and_open()
 except Exception as gen_error:
 	print("Attention, " + str(gen_error))
 	input("Press any key to exit..")
