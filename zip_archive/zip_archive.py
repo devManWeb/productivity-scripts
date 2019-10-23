acfa78018d424bd995d816a641b96043143b44dd
Leonardo Bissaro <leonbissaro@gmail.com>
2019-10-23 23:13:37 +0200

    added a module

--- zip_archive/zip_archive.py
+++ zip_archive/zip_archive.py
@@ -3,15 +3,15 @@ This script creates a zip archive of a list of selected folders
 The zip name file is the union of the current year, month, day, hours and minutes
 If, in the target directory, there are saved backups we choose if keep them or not
 All configurations of this script are found on the INI file 
-Important: this script was developed only for Windows
+Important: this script was developed for Win only
 '''
 
-import shutil
+from shutil import copytree, make_archive, move, rmtree
 from configparser import ConfigParser
-import os
-from time import strftime,sleep
-import stat
+from os import path, remove, name, walk
+from time import strftime, sleep
 from glob import glob
+from tempfile import mkdtemp, gettempdir
 
 class Zip_maker():
 
@@ -19,119 +19,120 @@ class Zip_maker():
 		#reading of the configuration file
 		config = ConfigParser()
 		config.read('config_file.ini')
-		self.destination = config["DEFAULT"]["destination_folder"]
-		
+		self.destination = self.__replace_slash(config["DEFAULT"]["destination_folder"])
+		self.auto_remove_previous = config["DEFAULT"]["auto_remove_previous"]
+
 		#array of folder paths to backup
 		self.folder_name = []
-		for i in range(1,len(config.items('SOURCE'))):
+		self.num_folders = len(config.items('SOURCE')) - 1
+		for i in range(1,self.num_folders):
 			self.folder_name.append(config["SOURCE"]["folder_" + str(i)])
 			
 		#Other variables used in this class
-		self.compression_ok = True	
 		self.file_name = strftime("%Y%m%d-%H%M")
-		self.local_path = os.path.dirname(os.path.abspath(__file__))
+		#temporary folder, deleted when we close this scipt
+		self.full_temp_path = mkdtemp(prefix="zip-archive-")
+
+	def __temp_cleanup(self):
+		#private method, removes the temporary folder
+		print("\nRemoving temporary files...")
+		rmtree(self.full_temp_path)
+
+	def __replace_slash(self,full_path):
+		#private method, replace / with \\
+		return full_path.replace('/', '\\')
+
+	'''	
+	TODO: implement a method to clean up past tmp folders
+	use previous_backups and make it a private method
+	
+	def __check_tmp_folders(self):
+		temp_dir = gettempdir()
+		for dirs in walk(temp_dir):
+		#if "zip-archive-" in temp_dir
+			print(dirs)
+	'''
 
 	def copy_compress(self):
-		partial = ""
+		#if not Win, we raise an error
+		if name != "nt":
+			raise Exception("OS not supported!")
+
 		#we copy all the content from the specified paths
-		for path in self.folder_name:
-			print("Copying " + path + "...\n")
-			partial = os.path.basename(path)
-			shutil.copytree(path,self.local_path + '\\temp\\' + partial)
+		partial = ""
+		for current_path in self.folder_name:
+			print("\nCopying " + current_path + "...")
+			partial = path.basename(current_path)
+			copytree(current_path,self.full_temp_path + '\\' + partial)
+
 		#making a zip archive	
-		print("Compressing " + self.file_name +".zip ...\n")
-		shutil.make_archive(self.file_name,"zip","temp")
+		print("\nCompressing " + self.file_name +".zip ...")
+		make_archive(
+			base_dir= self.full_temp_path,
+			root_dir= self.full_temp_path,
+			format='zip',
+			base_name= self.full_temp_path + "\\" + self.file_name 
+		)
 
-	def get_compression_ok(self):
-		return self.compression_ok
-		
-	def set_compression_ok(self,bool):
-		self.compression_ok = bool
-
-	def remove_temp_zip(self):
-		#we delete the zip and the temp folder if there is any error
-		try:
-			print("Deleting " + self.file_name +".zip ...\n")
-			os.remove(self.file_name +".zip ...")
-		except:
-			pass
-		
-		#this part is for removing the temp folder
-		print("Deleting temporary files...\n")
-		temp_dir_array = []
-		def rm_files(dir_list, dir_path):
-			for file in dir_list:
-				try:
-					os.remove(dir_path + "/" + file)
-				except PermissionError:		
-					os.chmod(dir_path + "/" + file, stat.S_IWRITE) #fix for only r files
-					os.remove(dir_path + "/" + file)
-
-		def rmv_directory(dir_entry):
-			rm_files(dir_entry[2], dir_entry[0])
-			temp_dir_array.insert(0, dir_entry[0])
-
-		for directory in os.walk("temp"):
-			rmv_directory(directory)
-
-		for dir in temp_dir_array:
-			os.rmdir(dir)
-		
 	def previous_backups(self):
 		#list of available backups with a Windows path style
 		list = glob(self.destination +'/*-*.zip')
-		backup_list = [pathname.replace('\\', '/') for pathname in list] 
+		backup_list = [self.__replace_slash(pathname) for pathname in list] 
 		
-		 #if there are saved backups, we choose if we want to keep it
+		#if there are saved backups, we choose if we want to keep it
 		if backup_list:
-			print("Saved backups:\n")
-			for file in range(0,len(backup_list)):
-				print(str(file) + " - " + backup_list[file])
-			
-			def backups_remover():
-				user_choice = input("Do you want to delete backups in " + self.destination + "? (Y/N)\n")
-				if user_choice.upper() == "Y":
-					for file in backup_list:
-						os.remove(file) 
-					print("Backups removed\n")					
-				elif user_choice.upper() == "N":
-					print("previous backups have not been removed!\n")				
-				else:
-					print("Not valid choice!\n")
-					backups_remover()
-					
-			backups_remover()
+			if self.auto_remove_previous == "True":
+				for file in backup_list:
+					remove(file) 
+					print("\nBackups removed")
+			else:
+				print("Saved backups:\n")
+				for file in range(0,len(backup_list)):
+					print(str(file) + " - " + backup_list[file])
+
+				def backups_remover():
+					print("\nDo you want to delete backups in " + self.destination + "?")
+					user_choice = input("Your choice (Y/N): ")
+					if user_choice.upper() == "Y":
+						for file in backup_list:
+							remove(file) 
+						print("\nBackups removed")					
+					elif user_choice.upper() == "N":
+						print("\nPrevious backups have not been removed!")				
+					else:
+						print("\nNot valid choice!")
+						backups_remover()
+						
+				backups_remover()
 				
-	def move_and_exit(self):
-		print("Moving " + self.file_name +".zip to " + self.destination + "...")
-		shutil.move(self.file_name + '.zip', self.destination)
-		
-		#used only to delete temp at the end
-		self.remove_temp_zip()
-		
-		#reverse loop for autoclosing ath the end
+	def move_backup(self):
+		#move the backup to the desired folder
+		print("\nMoving " + self.file_name +".zip to " + self.destination + "...")
+		move(
+			self.full_temp_path + "\\" + self.file_name + '.zip',
+			self.destination
+		)
+
+	def exit_function(self):	
+		#Exit message with an autoclosing loop
+		self.__temp_cleanup()
 		for i in range(10, 0, -1):
 			print('Closing in {} seconds...'.format(i), end='\r') #redraw line
 			sleep(1)
 		exit()	
 
-data = Zip_maker()
-
-try:
+try:	
+	data = Zip_maker()
 	data.copy_compress()
-	
-except Exception as comp_error:
-        data.set_compression_ok(False)
-        print(comp_error)
-        data.remove_temp_zip()
 
-if data.get_compression_ok(): 	
+except Exception as comp_error:
+	print(comp_error)
+
+else:
 	try:
 		data.previous_backups()
-		data.move_and_exit()
-		
+		data.move_backup()	
 	except Exception as del_mov_error:
 		print(del_mov_error)
-		data.remove_temp_zip()
-		
-		
+
+data.exit_function()
